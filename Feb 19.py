9283929f607f488c9bc37c992548aa858801c975
ConnorP898 <c.paniura.31415926@gmail.com>
2025-02-19 11:02:28 -0700

    Create Feb 19.py

---
+++ Feb 19.py
@@ -0,0 +1,16 @@
+## story made using variables, i couldn't put the varibles in the sentences so it looks a bit weird ##
+print (""""Welcome to the adventure simliator. I am going to ask a couple questions""")
+yourName = input ("What is your name?")
+superPower = input ("What is your superpower?")
+worstEnemy = input ("What is your worst enemy’s name?")
+favoriteFood = input ("What is your favorite food?")
+live = input ("Where do you live?")
+
+print("Hello", yourName, "Your ability to", superPower,)
+print ("will make sure you never have to deal with", worstEnemy, "again")
+print ("Go eat some", favoriteFood, "as you walk down the streets of", live,)
+
+## COLOURED TEXT! ##
+
+input ("PRESS ENTER")
+print ("Story:", "\033[32m","COMPLETE")
