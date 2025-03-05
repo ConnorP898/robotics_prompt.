780614e3be6360e8b149899afebce7f63dfd390d
ConnorP898 <c.paniura.31415926@gmail.com>
2025-03-05 10:44:58 -0700

    Create Mar 5.py

---
+++ Mar 5.py
@@ -0,0 +1,42 @@
+myName = input("What's your name? ")
+if myName == "connor":
+  print("welcome!")
+  print("you're the smartest person I've ever met")
+else:
+    print("who on earth are you?")
+print( )
+
+catsOrDogs = input("Are you a cat person? Or a dog person? ")
+if catsOrDogs == "cat":
+  print("meow")
+else:
+  print ("woof")
+print( )
+
+drink = input("do you prefer coffee or tea? ")
+if drink == "coffee":
+  print("tea is better")
+else:
+  print("good choice")
+print( )
+
+## challenge ##
+print("lets see how much you have in common with me")
+myName = input("hello, what is your name? ")
+print("hello", myName)
+chess = input("do you play chess? ")
+if chess == "yes":
+  Opening = input("good, what is your favourite opening? ")
+  if Opening == "vienna":
+    print("same")
+else:
+  print("you should try it")
+print( )
+
+album = input("what is your favourite album? ")
+if album=="the college dropout" or album=="late registration" or album=="graduation":
+  print("same, you have good tast")
+else:
+  print("cool")
+  print( )
+print("thanks for playing")
