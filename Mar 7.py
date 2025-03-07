5b56f67a7532b32d49fdb4483dbe9860d146f5e2
ConnorP898 <c.paniura.31415926@gmail.com>
2025-03-07 09:08:23 -0700

    Create Mar 7.py

---
+++ Mar 7.py
@@ -0,0 +1,44 @@
+print ("SECURE LOGING")
+userName = input ( "Username > ")
+password = input ("Password > ")
+if userName == "mark" and password == "password":
+  print ("Welcome Mark!")
+elif userName == "connor" and password == "Password":
+  print("Hey there Connor!")
+else: 
+  print ("go away!")
+
+## Common Errors ##
+print("SECURE LOGIN")
+username = input("Username > ")
+if username == "mark":
+  print("Welcome Mark!")
+elif username == "suzanne":
+  print("Hey there Suzanne!")
+else:
+  print("Go away!")
+  ## the problem was that the elif statement was after the esle statement ##
+
+## Fixing Code ##
+season = input("what is your favorite season?")
+if season == "spring"
+  print("Ah! The birds are chirping and flowers blooming.")
+elif season == "summer":
+  print("Catch some sun and cool off with a lemonade.")
+elif season == "autumn"
+  print("The leaves are changing and the air is crisp. Enjoy!")
+elif season == "winter":
+  print("Stay warm by the fire and watch the snow fall.")
+else: 
+  print("I don't know that season. Please try again.")
+  ## the code didn't have proper indentatiion, quotations, and the "==" ##
+
+## Day 6 Challenge ##
+print("SECURE LOGIN")
+print( )
+username = input("Username:  ")
+password = input("Password:  ")
+if username == "connor" and password == "password":
+  print( )
+  print("\033[32mCORRECT\033[0m")
+  print("Welcome Connor!")
